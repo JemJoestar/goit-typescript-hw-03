6cb7c7e510c9939c2a97200101a71c4799b48f50
JemJoestar <jemapro2@gmail.com>
2023-11-27 13:38:21 +0200

    done

--- src/3.ts
+++ src/3.ts
@@ -7,7 +7,20 @@
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
-// реалізація класу Wizard
+
+interface ICharacter {
+  name: string;
+  level: number;
+  
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+
+// Реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -29,11 +42,40 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
+// Тестування класу
 const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
+wizard.levelUp();  
 
-export {};
+// // реалізація класу Wizard
+// class Wizard implements ICharacter, ISpellCaster {
+//   constructor(public name: string, public level: number) {
+//     if (this.level < 1) {
+//       throw new Error('Level too low');
+//     }
+//   }
+
+//   introduce(phrase: string): void {
+//     console.log(phrase + ', ' + this.name);
+//   }
+
+//   castSpell(): void {
+//     console.log('Casting a spell, behold my power!');
+//   }
+
+//   levelUp(): void {
+//     this.level++;
+//     console.log(`Level up! New level is ${this.level}`);
+//   }
+// }
+
+// // тестування класу
+// const wizard = new Wizard('Merlin', 15);
+
+// wizard.introduce('I am the mighty wizard');
+// wizard.castSpell();
+// wizard.levelUp();  // Level up! New level is 16
+
+// export {};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,82 @@
-const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
 
-house.openDoor(person.getKey());
+// Клас Key
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  // Клас Person
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  // Абстрактний клас House
+  abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person entered the house.');
+      } else {
+        console.log('The door is closed. Cannot enter.');
+      }
+    }
+  }
+  
+  // Клас MyHouse успадковується від абстрактного класу House
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is open.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+  }
+  
+  // Сценарій
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
 
-house.comeIn(person);
+
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
 
 
 export {};
